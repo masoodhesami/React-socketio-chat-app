0438769c8a8fac90a5d1fc6e319c1e0ac2c3b115
Masoud Hesami <masoodhesami79@gmail.com>
2022-01-14 17:52:07 +0330

    bugs fixed

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -33,7 +33,7 @@ const Chat = ({socket, username, room}) => {
                 <ScrollToBottom className={"message-container"}>
                     {messageList.map((messageContent) => {
                         return (
-                            <div
+                            <div key={messageContent.message + Math.random().toString(16).slice(2)}
                                 className="message"
                                 id={username === messageContent.author ? "you" : "other"}
                             >
